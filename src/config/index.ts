8775254a4cfd9b6c8622d12dcc77b493d48e4322
Matias <matiasmarin45@gmail.com>
2022-01-04 08:38:09 -0300

    Cambios .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,8 @@ export default {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secreto',
   TOKEN_KEEP_ALIVE: process.env.TOKEN_KEEP_ALIVE || '1h',
   port: process.env.PORT || 8080,
-  username: process.env.USERNAME || 'root',
+  username: process.env.USERNAME_DATABASE || 'root',
   password: process.env.PASSWORD || 'yourPassword',
-  databaseName: process.env.DATABASE_NAME || 'dataBaseName',
   host: process.env.HOST || 'localhost',
+  databaseName: process.env.DATABASE_NAME || 'dataBaseName ',
 };
